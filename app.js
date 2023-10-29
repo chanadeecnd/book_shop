32e99918e33c7b4ce061c22782744b494d36032f
chanadeecnd <chanadeehk4082@gmail.com>
2023-10-30 01:08:42 +0700

    add url render

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:client_id,
     clientSecret:client_secret,
-    callbackURL: "http://localhost:3000/auth/google/home",
+    callbackURL: "http://localhost:3000/auth/google/home" || "https://nodejs-readdi.onrender.com/auth/google/home",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function (accessToken, refreshToken, profile, cb) {
